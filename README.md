8639c81b9e9051c111dcc31c138bdcd51bc65d1b
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-15 09:02:39 -0300

    Preenchendo repositório de Design Patterns

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Estudos de Design Patterns - GOF em Python
  Repositório de Estudos de POO Avançado em Python
+
+ Aulas Ministradas pelo Professor Luiz Otávio Miranda
---
+++ singleton_2.py
@@ -0,0 +1,30 @@
+def singleton(the_class):
+    instances = {}
+    
+    def get_class(*args, **kwargs):
+        if the_class not in instances:
+            instances[the_class] = the_class(*args, **kwargs)
+        return instances[the_class]
+    return get_class
+
+@singleton
+class AppSettings:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def __init__(self) -> None:
+        """O init será chamado todas as vezes"""
+        self.tema = 'O tema escuro'
+        self.font = '18px'
+
+if __name__ == '__main__':
+    as1 = AppSettings()
+    as1.tema = 'O tema claro'
+    print(as1.tema)
+
+    as2 = AppSettings()
+    print(as1.tema)
